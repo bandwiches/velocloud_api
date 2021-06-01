85a16cfba40e23808879d78879752c45e1bef433
Brett Hoshaw <brettimus.hoshaw@gmail.com>
2021-05-31 20:02:19 -0500

    documenting

--- velocloud/models/session.py
+++ velocloud/models/session.py
@@ -4,6 +4,12 @@ class VCO(object):
     """Velocloud Orchestrator"""
 
     def __init__(self, TOKEN: str, API_URL: str):
+        """Velocloud Orchestrator
+
+        Args:
+            TOKEN (str): A VCO Token
+            API_URL (str): A valid VCO URL
+        """
         self.token = f'TOKEN {TOKEN}'
         self.API_URL = API_URL
         self.headers = {'Content-Type': 'application/json', 'Authorization': f'{self.token}'}
